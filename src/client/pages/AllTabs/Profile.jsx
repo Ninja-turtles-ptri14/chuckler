c32df2eb87c899f54426ded747e92ad7b07ce82b
dpark001 <dparkzero1@gmail.com>
2024-05-01 22:11:23 -0700

    prep for change profile pic feature

--- src/client/pages/AllTabs/Profile.jsx
+++ src/client/pages/AllTabs/Profile.jsx
@@ -3,15 +3,22 @@ import '../../stylesheets/profile.css'
 import userProfileIcon from '../../images/userProfile.png';
 import {useNavigate} from 'react-router-dom';
 
-export default function Profile({userData}) {
-    return (
-        <div className="profile-container">
-            <img src={userProfileIcon} alt="user-profile"  style={{width:'370px'}}/>
-            <div className="info">
-                <p>Username: {userData.username} </p>
-                <p>User ID: {userData.id}</p>
-            </div>
-            {/* <button onClick={() => navigate}>Metrics</button> */}
-        </div>
-    )
+export default function Profile({ userData }) {
+  const changeProPic = async (e) => {
+    const res = await fetch('/api/user/profile')
+  }
+
+  return (
+    <div className="profile-container">
+      <div className="propic">
+        <img src={userProfileIcon} alt="user-profile" style={{ width: '370px' }} />
+        <button onClick={changeProPic}      
+      </div>
+      <div className="info">
+        <p>Username: {userData.username}</p>
+        <p>User ID: {userData.id}</p> 
+      </div>
+          {/* <button onClick={() => navigate}}>Metrics</button> */} 
+    </div>
+  );
 }
