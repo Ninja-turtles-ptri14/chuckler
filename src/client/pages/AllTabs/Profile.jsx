0f91e9bec6e6d9b8e65704418da27498cd34e2ac
dpark001 <dparkzero1@gmail.com>
2024-05-04 09:45:14 -0700

    change profile picture

--- src/client/pages/AllTabs/Profile.jsx
+++ src/client/pages/AllTabs/Profile.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import '../../stylesheets/profile.css'
 import userProfileIcon from '../../images/userProfile.png';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate, } from 'react-router-dom';
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Profile({ userData }) {
-  const changeProPic = async (e) => {
-    const res = await fetch('/api/user/profile')
-  }
+    const [profileImage, setProfileImage] = useState(userProfileIcon);
+    
+    // const form = document.getElementById('setImg');
+    // const img = form.get('image');
 
-  return (
-    <div className="profile-container">
-      <div className="propic">
-        <img src={userProfileIcon} alt="user-profile" style={{ width: '370px' }} />
-        <button onClick={changeProPic}      
-      </div>
-      <div className="info">
-        <p>Username: {userData.username}</p>
-        <p>User ID: {userData.id}</p> 
-      </div>
-          {/* <button onClick={() => navigate}}>Metrics</button> */} 
-    </div>
-  );
+    const { user } = useContext(AuthContext);
+    const token = user.token;
+    const header = { headers: { Authorization: `Bearer ${token}` } };
+    
+    const selectImg = (e) => {
+        const img = e.target.files;
+        setProfileImage(img);
+    }
+
+    const changeProImg = async (e) => {
+        // const newImg = new FormData();
+        // newImg.append('image', profileImage);
+        // console.log('this is new FormData: ', newImg);
+
+        try {
+            const res = await fetch('/api/user/picture', {
+                method: 'POST',
+                body: JSON.stringify({ image: profileImage }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                // Authorization: `Bearer ${token}`
+            });
+        } catch (err) {
+            console.error(`Could not set new profile image. Error: ${err}`);
+        }
+    };
+
+    return (
+        <div className="profile-container">
+            <div className="pro-img">
+                <img
+                    src={profileImage}
+                    alt="user-profile"
+                    style={{ width: '370px' }}
+                />
+                <div>
+                    {/* <form id="setImg">
+                        <label htmlFor="image">Set new profile image</label>
+                        <input type="file" accept="image/*" id="image" onChange={selectImg} />
+                        <button type="submit" onClick={changeProImg}>Change image</button>
+                    </form> */}
+                    <label htmlFor="image">Change profile picture</label>
+                    <input id="image" type="file" accept="image/*" onChange={selectImg} />
+                    <button onClick={changeProImg}>Ok</button>
+                </div>
+            </div>
+            <div className="info">
+                <p>Username: {userData.username}</p>
+                <p>User ID: {userData.id}</p> 
+            </div>
+            {/* <button onClick={() => navigate}}>Metrics</button> */} 
+        </div>
+    );
 }
--- src/client/pages/Main.jsx
+++ src/client/pages/Main.jsx
@@ -10,22 +10,23 @@ import TabNavItem from "./Components/TabNavItem.jsx";
 import Axios from "axios";
 
 export default function Main({ userData }) {
-  const [file,setFile] = useState();
-  const [caption, setCaption] = useState("");
+  // const [file,setFile] = useState();
+  // const [caption, setCaption] = useState("");
 
-  const submit = async event =>{
-    event.preventDefault()
+  // const submit = async event =>{
+  //   event.preventDefault()
 
-    const formData = new FormData();
-    formData.append("image",file);
-    formData.append("caption",caption)
-    await Axios.post('/api/user/picture',formData,{headers:{"Content-Type":"multipart/form-data"}})
-  }
+  //   const formData = new FormData();
+  //   formData.append("image",file);
+  //   formData.append("caption",caption)
+  //   await Axios.post('/api/user/picture',formData,{headers:{"Content-Type":"multipart/form-data"}})
+  // }
 
-  const fileSelected = e =>{
-    const file = e.target.files[0]
-    setFile(file);
-  }
+  // const fileSelected = e =>{
+  //   const file = e.target.files[0]
+  //   setFile(file);
+  // }
+
   const [activeTab, setActiveTab] = useState("tab1");
   // fetch('/api/user/verify').then(data=>data.text()).then(data=>{if(data==='false') location.assign('/')})
   const handleLogOut = async (e) => {
@@ -78,11 +79,11 @@ export default function Main({ userData }) {
           />
         </ul>
       </div>
-      <form onSubmit={submit}>
+      {/* <form onSubmit={submit}>
         <input onChange={fileSelected} type="file" accept="image/*"></input>
         <input value={caption} onChange={e=>setCaption(e.target.value)}type="text"></input>
         <button type='submit'>Submit</button>
-      </form>
+      </form> */}
     </div>
   )
 }
